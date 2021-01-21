8ac4e2dda9cab62abec54dc31127e2bddba4df9b
carlacentenor <carlacentenor@gmail.com>
2021-01-21 12:26:05 -0500

    add images

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 let slider = document.querySelector('.brands-js');
 
+const mediaQuery = window.matchMedia('(max-width: 767px)');
+if (mediaQuery.matches) {
+  
+    $('.brands-slider-mobile').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: true,
+        centerPadding: '60px',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
 
-$(document).ready(function () {
-    if(window.matchMedia("(max-width: 768px)")){
-        $('.brands-slider-mobile').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            centerMode: true,
-            centerPadding: '60px',
-            autoplay: true,
-            autoplaySpeed: 2000,
-            arrows: false
-    
-        });
-    }
-    else{
-        $('.brands-slider-mobile').slick('unslick');
-    }
-   
-});
+    });
+
+  }
+
